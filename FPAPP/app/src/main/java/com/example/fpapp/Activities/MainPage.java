aabe87bb7fe2eee0cd785d620c697d0e318ffc43
NateChenXJ <natechenfly@gmail.com>
2018-04-02 23:12:10 -0600

    Done Configuration Merge
    Merge

--- FPAPP/app/src/main/java/com/example/fpapp/Activities/MainPage.java
+++ FPAPP/app/src/main/java/com/example/fpapp/Activities/MainPage.java
@@ -1,39 +1,78 @@
 package com.example.fpapp.Activities;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Switch;
-
-import android.content.Intent;
 
 import com.example.fpapp.R;
 
-/**
- * Created by chenxiaojie on 2017-10-29.
- */
 
 public class MainPage extends AppCompatActivity {
 
     private TextView textViewName;
 
-    private Button location, camera;
-
-
+    private ImageView led, fan, configuration, lock;
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_page);
+        setContentView(R.layout.activity_main_page);
 
         textViewName = (TextView) findViewById(R.id.name);
         String nameFromIntent = getIntent().getStringExtra("EMAIL");
         textViewName.setText("Welcome " + nameFromIntent);
 
-
+//        led();
+//        fan();
+        configuration();
+//        lock();
     }
 
+//    public void led(){
+//        led = (ImageView) findViewById(R.id.led);
+//        led.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v){
+//                Intent intent0 = new Intent(MainActivity.this, MainLed.class);
+//                startActivity(intent0);
+//            }
+//        });
+//    }
+//
+//    public void fan(){
+//        fan = (ImageView) findViewById(R.id.fan);
+//        fan.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v){
+//                Intent intent1 = new Intent(MainActivity.this, MainFan.class);
+//                startActivity(intent1);
+//            }
+//        });
+//    }
+
+    public void configuration(){
+        configuration = (ImageView) findViewById(R.id.configuration);
+        configuration.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent2 = new Intent(MainPage.this, MainConfiguration.class);
+                startActivity(intent2);
+            }
+        });
+    }
+
+//    public void lock(){
+//        lock = (ImageView) findViewById(R.id.lock);
+//        lock.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v){
+//                Intent intent3 = new Intent(MainActivity.this, MainLock.class);
+//                startActivity(intent3);
+//            }
+//        });
+//    }
 
 
 }
+
